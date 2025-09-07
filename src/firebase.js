f9a20a7ffd7b2fdb8c776d50ad1dc1a00dbcbc20
Simran Verma <simranverma.sv17@gmail.com>
2025-09-07 17:20:32 +0530

    Fixed the firebase issue.

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDOfkxUAmFxqLIqYxyG6aanYbMZuTI3krA",
-  authDomain: "blogsphere-67bf5.firebaseapp.com",
-  projectId: "blogsphere-67bf5",
-  storageBucket: "blogsphere-67bf5.appspot.com",
-  messagingSenderId: "390712781339",
-  appId: "1:390712781339:web:33836e69ada15d2810a6be",
-  measurementId: "G-V3PKH3BN95"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // must exist if used
-export default app
+export const storage = getStorage(app);
+export default app;
